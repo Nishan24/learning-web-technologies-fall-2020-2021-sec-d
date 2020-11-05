84d5d78b8aa6bae6f678e40a14ef7c78dfbebd87
Nishan Das <nishan.sid@gmail.com>
2020-11-05 10:53:24 +0600

    1st commit

---
+++ LAB_TASK_4.1_SESSION/homepage.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>XCompany - Home</title>
+</head>
+<body>
+    
+    <form action="">
+        
+        <table border="2" width="60%">
+            
+            <tr>
+                
+                <td>
+                    
+                    <img src="img/logo.png" alt="Logo">
+                    
+                   
+                    
+                </td>
+                
+                <td align="right">
+                    
+                    
+				<a href ="homepage.php" target="blank"> Home</a> |
+                        <a href ="Login.php" target="blank"> Login </a> |
+						<a href ="Registration" target="blank"> Registration </a> 
+                    
+                </td>
+ 
+                
+            </tr>
+            
+            <tr>
+                
+                <td colspan="2" height=200px><h2>Welcome to xCompany</h2></td>
+                
+            </tr>
+            
+            <tr>
+                
+                <td colspan="2" align="center"><p>Copyright &#169; 2017</p></td>
+                
+            </tr>
+            
+        </table>
+        
+    </form>
+    
+</body>
+</html>
