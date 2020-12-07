7dcc7690d4542449187737c9a9e8a8c3a177944f
Nishan Das <nishan.sid@gmail.com>
2020-12-07 10:31:35 +0600

    Gender and Degree

--- FINAL_LAB_TASK_JS_3.1/validation.js
+++ FINAL_LAB_TASK_JS_3.1/validation.js
@@ -70,3 +70,36 @@ function emailValidation(){
         
     }
 }
+
+function genderValidation(){
+    
+    if(document.getElementById("male").checked|| document.getElementById("female").checked|| document.getElementById("other").checked){
+        
+        alert("Gender is selected");
+        
+    }
+    
+    else{
+        
+        alert("select one gender");
+        
+    }
+    
+}
+
+function degreeValidation(){
+    
+    if(document.getElementById("ssc").checked|| document.getElementById("hsc").checked|| document.getElementById("Bsc").checked){
+        
+        alert("Degree is selected");
+        
+    }
+    
+    else{
+        
+        alert("select one degree");
+        
+    }
+    
+}
+
